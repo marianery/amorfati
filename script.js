f394b6fd4ece84efb6aeae0475358f0b48e040f6
marianery <48214519+marianery@users.noreply.github.com>
2020-04-09 13:20:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "50%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+   $('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  wrapAround: true
+});
