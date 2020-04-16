049ef5427d7fed7a1c0948df4cae2c8fc1baaa0e
marianery <48214519+marianery@users.noreply.github.com>
2020-04-16 09:48:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("mySidenav").style.border = "0px solid";
-  document.getElementById("myText").style.width = "386px";
+  document.getElementById("myText").style.width = "70%";
   modal.style.display = "none";
 }
 
